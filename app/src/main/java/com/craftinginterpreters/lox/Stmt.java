7b7c8677c5adaffc3614a0135b644395d57b5c96
arid-mangoss <sssnarr1@gmail.com>
2022-12-12 22:22:37 -0500

    ch 8 statements and state

--- app/src/main/java/com/craftinginterpreters/lox/Stmt.java
+++ app/src/main/java/com/craftinginterpreters/lox/Stmt.java
@@ -4,11 +4,25 @@ import java.util.List;
 
 abstract class Stmt {
     interface Visitor<R> {
+        R visitBlockStmt(Block stmt);
         R visitExpressionStmt(Expression stmt);
         R visitPrintStmt(Print stmt);
+        R visitVarStmt(Var stmt);
     }
 
     abstract <R> R accept(Visitor<R> visitor);
+    static class Block extends Stmt {
+        final List<Stmt> statements;
+
+        Block(List<Stmt> statements) {
+            this.statements = statements;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBlockStmt(this);
+        }
+    }
     static class Expression extends Stmt {
         final Expr expression;
 
@@ -31,6 +45,20 @@ abstract class Stmt {
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitPrintStmt(this);
+        }
+    }
+    static class Var extends Stmt {
+        final Token name;
+        final Expr initializer;
+
+        Var(Token name, Expr initializer) {
+            this.name = name;
+            this.initializer = initializer;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVarStmt(this);
         }
     }
 }
